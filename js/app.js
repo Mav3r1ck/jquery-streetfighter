8bdc3a5f2f387ecd7f16a93fe47e29585be625fa
Aaron <Aaron@Mav3r1ck.home>
2015-12-11 18:32:42 -0500

    What PITA
    WOOT WOOT

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+
+  .mousedown(function() {
+  	playHadouken();
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').finish().show()
+  
+  .animate({'left': '1020px'},500, function() {
+      $(this).hide();
+      $(this).css('left', '520px');
+     }
+  	);
+  })
+
+  .mouseup(function() {
+    console.log('mouseup');
+    $('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  });
+
+  $(document).keydown(function(down) {
+    if (down.keyCode == 88) {
+      $('.ryu-action').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }   
+  });
+
+    $(document).keyup(function(up) {
+    if (up.keyCode == 88) {
+      $('.ryu-ready').show();
+      $('.ryu-cool').hide();
+      $('.ryu-still').hide();
+    }   
+  });
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+  }
